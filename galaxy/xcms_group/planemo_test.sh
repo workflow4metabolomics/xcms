fada4bbc29ff13c5adb2ccd20083d6ecbcd70ea0
lecorguille <lecorguille@sb-roscoff.fr>
2016-04-07 12:29:06 +0200

    add a second test to group using retcor output

---
+++ galaxy/xcms_group/planemo_test.sh
@@ -0,0 +1,7 @@
+planemo conda_init
+planemo conda_install .
+planemo test --install_galaxy --conda_dependency_resolution --galaxy_branch "dev"
+
+#All 2 test(s) executed passed.
+#abims_xcms_group[0]: passed
+#abims_xcms_group[1]: passed
