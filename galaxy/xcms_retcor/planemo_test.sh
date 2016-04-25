727b4a74b8e424af622dc0e2b0c910cdd020cd29
lecorguille <lecorguille@users.noreply.github.com>
2016-04-25 16:17:26 +0200

    minor changes in planemo_test.sh

--- galaxy/xcms_retcor/planemo_test.sh
+++ galaxy/xcms_retcor/planemo_test.sh
@@ -1,11 +1,10 @@
-planemo conda_init
-planemo conda_install .
-planemo test --install_galaxy --conda_dependency_resolution --galaxy_branch "dev"
+# Example of planemo command to launch test
 
-#All 1 test(s) executed passed.
-#abims_xcms_retcor[0]: passed
+# Note: --galaxy_branch "dev" is set to deal with zip file
 
 
+# -- Use of installed package environments
+# after having installing package on a local galaxy instance
 source /w/galaxy/dev/shed_tools_tool_dependency_dir/R/3.1.2/iuc/package_r_3_1_2/1ca39eb16186/env.sh
 source /w/galaxy/dev/shed_tools_tool_dependency_dir/bioconductor-xcms/1.44.0/lecorguille/package_bioconductor_xcms_1_44_0/0c38f7d43e08/env.sh
 planemo test --install_galaxy --galaxy_branch "dev"
@@ -14,6 +13,16 @@ planemo test --install_galaxy --galaxy_branch "dev"
 #abims_xcms_retcor[0]: passed
 
 
+# -- Use of conda dependencies
+planemo conda_init --conda_prefix /tmp/mc
+planemo conda_install --conda_prefix /tmp/mc . 
+planemo test --install_galaxy --conda_prefix /tmp/mc --conda_dependency_resolution --galaxy_branch "dev"
+
+#All 1 test(s) executed passed.
+#abims_xcms_retcor[0]: passed
+
+
+# -- Use of shed_test
 planemo shed_test --install_galaxy --galaxy_branch "dev" -t testtoolshed
 #All 1 test(s) executed passed.
 #testtoolshed.g2.bx.psu.edu/repos/lecorguille/xcms_retcor/abims_xcms_retcor/2.0.6[0]: passed
--- galaxy/xcms_summary/planemo_test.sh
+++ galaxy/xcms_summary/planemo_test.sh
@@ -1,10 +1,9 @@
-planemo conda_init
-conda install bioconductor-camera r-batch
-
-#All 1 test(s) executed passed.
-#abims_xcms_summary[0]: passed
+# Example of planemo command to launch test
 
 
+
+# -- Use of installed package environments
+# after having installing package on a local galaxy instance
 source /w/galaxy/dev/shed_tools_tool_dependency_dir/R/3.1.2/iuc/package_r_3_1_2/1ca39eb16186/env.sh
 source /w/galaxy/dev/shed_tools_tool_dependency_dir/bioconductor-camera/1.22.0/lecorguille/package_bioconductor_camera_1_22_0/22cec61d66c2/env.sh
 planemo test --install_galaxy
@@ -13,6 +12,16 @@ planemo test --install_galaxy
 #abims_xcms_summary[0]: passed
 
 
+# -- Use of conda dependencies
+planemo conda_init --conda_prefix /tmp/mc
+planemo conda_install --conda_prefix /tmp/mc . 
+planemo test --install_galaxy --conda_prefix /tmp/mc --conda_dependency_resolution
+
+#All 1 test(s) executed passed.
+#abims_xcms_summary[0]: passed
+
+
+# -- Use of shed_test
 planemo shed_test --install_galaxy
 
 #All 1 test(s) executed passed.
