2c8dafbe57419cc5bed31bdb5968e645b2ddcaac
lecorguille <lecorguille@users.noreply.github.com>
2016-08-04 15:42:03 +0200

    bugfix ; xcmsSet version

--- src/xcms_w4m_script/xcms.r
+++ src/xcms_w4m_script/xcms.r
@@ -108,7 +108,8 @@ if (!is.null(listArguments[["zipfile"]])){
 }
 
 if (!is.null(listArguments[["singlefile_galaxyPath"]])){
-  singlefile = list(listArguments[["singlefile_sampleName"]] = listArguments[["singlefile_galaxyPath"]]); listArguments[["singlefile_galaxyPath"]]=NULL; listArguments[["singlefile_sampleName"]]=NULL
+  singlefile = list()
+  singlefile[[listArguments[["singlefile_sampleName"]]]] = listArguments[["singlefile_galaxyPath"]]); listArguments[["singlefile_galaxyPath"]]=NULL; listArguments[["singlefile_sampleName"]]=NULL
 }
 
 if (!is.null(listArguments[["library"]])){
