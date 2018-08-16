0c13258bbdb62ed37dcf0a3b173b58f9c493b543
lecorguille <gildas.le.corguille@gmail.com>
2018-08-16 16:14:35 +0200

    fix test extension for RData ; enhance getxcmsSetObject function ; add a toy R script with all the commands

---
+++ tools/scripts/faahKO_4datasets.r
@@ -0,0 +1,80 @@
+library(xcms)
+library(CAMERA)
+library(faahKO)
+
+cdfs <- dir(system.file("cdf", package = "faahKO"), full.names = TRUE,
+        recursive = TRUE)
+
+cdfs <- cdfs[c(1,2,7,8)]
+
+pd <- data.frame(
+        sample_name = sub(basename(cdfs), pattern = ".CDF",
+        replacement = "", fixed = TRUE),
+        sample_group = c(rep("KO", 2), rep("WT", 2)),
+        stringsAsFactors = FALSE)
+
+raw_data <- readMSData(files = cdfs, pdata = new("NAnnotatedDataFrame", pd),
+        mode = "onDisk")
+
+cwp <- CentWaveParam()
+xdata <- findChromPeaks(raw_data, param = cwp)
+
+pdp <- PeakDensityParam(sampleGroups = xdata$sample_group,
+        minFraction = 0.8)
+xdata <- groupChromPeaks(xdata, param = pdp)
+
+pgp <- PeakGroupsParam(minFraction = 0.85)
+xdata <- adjustRtime(xdata, param = pgp)
+
+pdp <- PeakDensityParam(sampleGroups = xdata$sample_group,
+        minFraction = 0.4, bw = 20)
+xdata <- groupChromPeaks(xdata, param = pdp)
+
+xdata <- fillChromPeaks(xdata)
+
+# This function retrieve a xset like object
+#@author Gildas Le Corguille lecorguille@sb-roscoff.fr
+getxcmsSetObject <- function(xobject) {
+    # XCMS 1.x
+    if (class(xobject) == "xcmsSet")
+        return (xobject)
+    # XCMS 3.x
+    if (class(xobject) == "XCMSnExp") {
+        # Get the legacy xcmsSet object
+        suppressWarnings(xset <- as(xobject, 'xcmsSet'))
+        if (is.null(xset@phenoData$sample_group))
+            sampclass(xset) = "."
+        else
+            sampclass(xset) <- xset@phenoData$sample_group
+        return (xset)
+    }
+}
+
+xset <- getxcmsSetObject(xdata)
+
+xa <- annotate(
+        xset,
+        nSlaves = 1,
+        sigma = 6,
+        perfwhm = 0.6,
+        ppm = 5,
+        mzabs = 0.015,
+        maxcharge = 3,
+        maxiso = 4,
+        minfrac = 0.5,
+        quick = TRUE,
+        intval = "into")
+
+diffrep <- diffreport(
+        object=xset,
+        class1="KO",
+        class2="WT",
+        filebase="KO-vs-WT",
+        eicmax=200,
+        eicwidth=200,
+        sortpval=TRUE,
+        value="into",
+        h=480,
+        w=640,
+        mzdec=2,
+        missing=0)
