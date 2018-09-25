5f1c45129d02106d12377047f6378511f3f6275e
INRA\mtremblayfr <marie.tremblay-franco@inra.fr>
2018-09-25 18:52:48 +0200

    Bug fixing for PQN normalization in case of zero's

--- galaxy/normalization/NmrNormalization_wrapper.R
+++ galaxy/normalization/NmrNormalization_wrapper.R
@@ -14,6 +14,7 @@ runExampleL <- FALSE
 ##------------------------------
 strAsFacL <- options()$stringsAsFactors
 options(stringsAsFactors = FALSE)
+options(scipen=999)
 
 
 ##------------------------------
