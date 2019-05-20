6ec94834cf918926f29c54e0fb8ccd023725f498
lecorguille <gildas.le.corguille@gmail.com>
2019-05-20 16:18:35 +0200

    script reactoring : pass 3

--- tools/scripts/lib.r
+++ tools/scripts/lib.r
@@ -103,32 +103,32 @@ annotatediff <- function(xset=xset, args=args, variableMetadataOutput="variableM
     res=try(is.null(xset@filled))
 
     # ------ annot -------
-    args[["calcCiS"]]=as.logical(args[["calcCiS"]])
-    args[["calcIso"]]=as.logical(args[["calcIso"]])
-    args[["calcCaS"]]=as.logical(args[["calcCaS"]])
+    args$calcCiS=as.logical(args$calcCiS)
+    args$calcIso=as.logical(args$calcIso)
+    args$calcCaS=as.logical(args$calcCaS)
 
     # common parameters
     args4annotate = list(object=xset,
-    nSlaves=args[["nSlaves"]],sigma=args[["sigma"]],perfwhm=args[["perfwhm"]],
-    maxcharge=args[["maxcharge"]],maxiso=args[["maxiso"]],minfrac=args[["minfrac"]],
-    ppm=args[["ppm"]],mzabs=args[["mzabs"]],quick=args[["quick"]],
-    polarity=args[["polarity"]],max_peaks=args[["max_peaks"]],intval=args[["intval"]])
+        nSlaves=args$nSlaves,sigma=args$sigma,perfwhm=args$perfwhm,
+        maxcharge=args$maxcharge,maxiso=args$maxiso,minfrac=args$minfrac,
+        ppm=args$ppm,mzabs=args$mzabs,quick=args$quick,
+        polarity=args$polarity,max_peaks=args$max_peaks,intval=args$intval)
 
     # quick == FALSE
-    if(args[["quick"]]==FALSE) {
+    if(args$quick==FALSE) {
         args4annotate = append(args4annotate,
-            list(graphMethod=args[["graphMethod"]],cor_eic_th=args[["cor_eic_th"]],pval=args[["pval"]],
-            calcCiS=args[["calcCiS"]],calcIso=args[["calcIso"]],calcCaS=args[["calcCaS"]]))
+            list(graphMethod=args$graphMethod,cor_eic_th=args$cor_eic_th,pval=args$pval,
+            calcCiS=args$calcCiS,calcIso=args$calcIso,calcCaS=args$calcCaS))
         # no ruleset
-        if (!is.null(args[["multiplier"]])) {
+        if (!is.null(args$multiplier)) {
             args4annotate = append(args4annotate,
-                list(multiplier=args[["multiplier"]]))
+                list(multiplier=args$multiplier))
         }
         # ruleset
         else {
-            rulset=read.table(args[["rules"]], h=T, sep=";")
-            if (ncol(rulset) < 4) rulset=read.table(args[["rules"]], h=T, sep="\t")
-            if (ncol(rulset) < 4) rulset=read.table(args[["rules"]], h=T, sep=",")
+            rulset=read.table(args$rules, h=T, sep=";")
+            if (ncol(rulset) < 4) rulset=read.table(args$rules, h=T, sep="\t")
+            if (ncol(rulset) < 4) rulset=read.table(args$rules, h=T, sep=",")
             if (ncol(rulset) < 4) {
                 error_message="Your ruleset file seems not well formatted. The column separators accepted are ; , and tabulation"
                 print(error_message)
@@ -143,12 +143,12 @@ annotatediff <- function(xset=xset, args=args, variableMetadataOutput="variableM
 
     # launch annotate
     xa = do.call("annotate", args4annotate)
-    peakList=getPeaklist(xa,intval=args[["intval"]])
+    peakList=getPeaklist(xa,intval=args$intval)
     peakList=cbind(groupnames(xa@xcmsSet),peakList); colnames(peakList)[1] = c("name");
 
     # --- Multi condition : diffreport ---
     diffrepOri=NULL
-    if (!is.null(args[["runDiffreport"]]) & nlevels(sampclass(xset))>=2) {
+    if (!is.null(args$runDiffreport) & nlevels(sampclass(xset))>=2) {
         #Check if the fillpeaks step has been done previously, if it hasn't, there is an error message and the execution is stopped.
         res=try(is.null(xset@filled))
         classes=levels(sampclass(xset))
@@ -159,7 +159,10 @@ annotatediff <- function(xset=xset, args=args, variableMetadataOutput="variableM
                 if(i+n <= length(classes)){
                     filebase=paste(classes[i],class2=classes[i+n],sep="-vs-")
 
-                    diffrep=diffreport(object=xset,class1=classes[i],class2=classes[i+n],filebase=filebase,eicmax=args[["eicmax"]],eicwidth=args[["eicwidth"]],sortpval=TRUE,value=args[["value"]],h=args[["h"]],w=args[["w"]],mzdec=args[["mzdec"]],missing=0)
+                    diffrep=diffreport(
+                        object=xset,class1=classes[i],class2=classes[i+n],
+                        filebase=filebase,eicmax=args$eicmax,eicwidth=args$eicwidth,
+                        sortpval=TRUE,value=args$value,h=args$h,w=args$w,mzdec=args$mzdec,missing=0)
 
                     diffrepOri = diffrep
 
@@ -171,35 +174,35 @@ annotatediff <- function(xset=xset, args=args, variableMetadataOutput="variableM
                     diffrep = merge(peakList, diffrep[,c("name","fold","tstat","pvalue")], by.x="name", by.y="name", sort=F)
                     diffrep = cbind(diffrep[,!(colnames(diffrep) %in% c(sampnames(xa@xcmsSet)))],diffrep[,(colnames(diffrep) %in% c(sampnames(xa@xcmsSet)))])
 
-                    diffrep = RTSecondToMinute(diffrep, args[["convertRTMinute"]])
-                    diffrep = formatIonIdentifiers(diffrep, numDigitsRT=args[["numDigitsRT"]], numDigitsMZ=args[["numDigitsMZ"]])
+                    diffrep = RTSecondToMinute(diffrep, args$convertRTMinute)
+                    diffrep = formatIonIdentifiers(diffrep, numDigitsRT=args$numDigitsRT, numDigitsMZ=args$numDigitsMZ)
 
-                    if(args[["sortpval"]]){
+                    if(args$sortpval){
                         diffrep=diffrep[order(diffrep$pvalue), ]
                     }
 
                     dir.create("tabular", showWarnings = FALSE)
                     write.table(diffrep, sep="\t", quote=FALSE, row.names=FALSE, file=paste("tabular/",filebase,"_tsv.tabular",sep=""))
 
-                    if (args[["eicmax"]] != 0) {
-                        if (args[["png2"]] == "pdf")
+                    if (args$eicmax != 0) {
+                        if (args$png2 == "pdf")
                             diffreport_png2pdf(filebase)
                     }
                 }
             }
         }
-        if (args[["png2"]] == "zip")
+        if (args$png2 == "zip")
             diffreport_png2zip()
-        if (args[["tabular2"]] == "zip")
+        if (args$tabular2 == "zip")
             diffreport_tabular2zip()
     }
 
     # --- variableMetadata ---
     variableMetadata=peakList[,!(make.names(colnames(peakList)) %in% c(make.names(sampnames(xa@xcmsSet))))]
-    variableMetadata = RTSecondToMinute(variableMetadata, args[["convertRTMinute"]])
-    variableMetadata = formatIonIdentifiers(variableMetadata, numDigitsRT=args[["numDigitsRT"]], numDigitsMZ=args[["numDigitsMZ"]])
+    variableMetadata = RTSecondToMinute(variableMetadata, args$convertRTMinute)
+    variableMetadata = formatIonIdentifiers(variableMetadata, numDigitsRT=args$numDigitsRT, numDigitsMZ=args$numDigitsMZ)
     # if we have 2 conditions, we keep stat of diffrep
-    if (!is.null(args[["runDiffreport"]]) & nlevels(sampclass(xset))==2) {
+    if (!is.null(args$runDiffreport) & nlevels(sampclass(xset))==2) {
         variableMetadata = merge(variableMetadata, diffrep[,c("name","fold","tstat","pvalue")],by.x="name", by.y="name", sort=F)
         if(exists("args[[\"sortpval\"]]")){
             variableMetadata=variableMetadata[order(variableMetadata$pvalue), ]
@@ -214,7 +217,9 @@ annotatediff <- function(xset=xset, args=args, variableMetadataOutput="variableM
 }
 
 
-combinexsAnnos_function <- function(xaP, xaN, diffrepP=NULL,diffrepN=NULL,pos=TRUE,tol=2,ruleset=NULL,keep_meta=TRUE, convertRTMinute=F, numDigitsMZ=0, numDigitsRT=0, variableMetadataOutput="variableMetadata.tsv"){
+combinexsAnnos_function <- function(xaP, xaN, diffrepP=NULL,diffrepN=NULL,
+    pos=TRUE,tol=2,ruleset=NULL,keep_meta=TRUE, convertRTMinute=F, numDigitsMZ=0,
+    numDigitsRT=0, variableMetadataOutput="variableMetadata.tsv"){
 
     #Load the two Rdata to extract the xset objects from positive and negative mode
     cat("\tObject xset from positive mode\n")
@@ -281,21 +286,21 @@ combinexsAnnos_function <- function(xaP, xaN, diffrepP=NULL,diffrepN=NULL,pos=TR
 
 # This function get the raw file path from the arguments
 getRawfilePathFromArguments <- function(singlefile, zipfile, args) {
-    if (!is.null(args[["zipfile"]]))           zipfile = args[["zipfile"]]
-    if (!is.null(args[["zipfilePositive"]]))   zipfile = args[["zipfilePositive"]]
-    if (!is.null(args[["zipfileNegative"]]))   zipfile = args[["zipfileNegative"]]
+    if (!is.null(args$zipfile))           zipfile = args$zipfile
+    if (!is.null(args$zipfilePositive))   zipfile = args$zipfilePositive
+    if (!is.null(args$zipfileNegative))   zipfile = args$zipfileNegative
 
-    if (!is.null(args[["singlefile_galaxyPath"]])) {
-        singlefile_galaxyPaths = args[["singlefile_galaxyPath"]];
-        singlefile_sampleNames = args[["singlefile_sampleName"]]
+    if (!is.null(args$singlefile_galaxyPath)) {
+        singlefile_galaxyPaths = args$singlefile_galaxyPath;
+        singlefile_sampleNames = args$singlefile_sampleName
     }
-    if (!is.null(args[["singlefile_galaxyPathPositive"]])) {
-        singlefile_galaxyPaths = args[["singlefile_galaxyPathPositive"]];
-        singlefile_sampleNames = args[["singlefile_sampleNamePositive"]]
+    if (!is.null(args$singlefile_galaxyPathPositive)) {
+        singlefile_galaxyPaths = args$singlefile_galaxyPathPositive;
+        singlefile_sampleNames = args$singlefile_sampleNamePositive
     }
-    if (!is.null(args[["singlefile_galaxyPathNegative"]])) {
-        singlefile_galaxyPaths = args[["singlefile_galaxyPathNegative"]];
-        singlefile_sampleNames = args[["singlefile_sampleNameNegative"]]
+    if (!is.null(args$singlefile_galaxyPathNegative)) {
+        singlefile_galaxyPaths = args$singlefile_galaxyPathNegative;
+        singlefile_sampleNames = args$singlefile_sampleNameNegative
     }
     if (exists("singlefile_galaxyPaths")){
         singlefile_galaxyPaths = unlist(strsplit(singlefile_galaxyPaths,","))
@@ -308,7 +313,10 @@ getRawfilePathFromArguments <- function(singlefile, zipfile, args) {
             singlefile[[singlefile_sampleName]] = singlefile_galaxyPath
         }
     }
-    for (argument in c("zipfile","zipfilePositive","zipfileNegative","singlefile_galaxyPath","singlefile_sampleName","singlefile_galaxyPathPositive","singlefile_sampleNamePositive","singlefile_galaxyPathNegative","singlefile_sampleNameNegative")) {
+    for (argument in c("zipfile", "zipfilePositive", "zipfileNegative",
+                        "singlefile_galaxyPath", "singlefile_sampleName",
+                        "singlefile_galaxyPathPositive", "singlefile_sampleNamePositive",
+                        "singlefile_galaxyPathNegative","singlefile_sampleNameNegative")) {
         args[[argument]]=NULL
     }
     return(list(zipfile=zipfile, singlefile=singlefile, args=args))
