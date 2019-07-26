0be5adce82620be5938bb0b524043dbee8de6fc7
lecorguille <gildas.le.corguille@gmail.com>
2019-07-26 17:23:19 +0200

    datatype - flake8

--- datatypes/datatype_rdata_camera/rdata_camera_datatype.py
+++ datatypes/datatype_rdata_camera/rdata_camera_datatype.py
@@ -6,32 +6,30 @@ Author: lecorguille@sb-roscoff.fr
 """
 
 import logging
-import os,os.path,re
-from galaxy.datatypes.data import *
-from galaxy.datatypes.sniff import *
-from galaxy.datatypes.binary import * 
+
+from galaxy.datatypes.binary import RData
 
 log = logging.getLogger(__name__)
-    
 
-class RdataCameraPositive( Binary ):
+
+class RdataCameraPositive(RData):
     file_ext = "rdata.camera.positive"
-    blurb = "Output of CAMERA.annotate using positif acquisition mode\nInput of ProbMetab or CAMERA.combinexcAnnos"
-    
-    #TODO: sniff
+    blurb = "Output of CAMERA.annotate using positif acquisition mode\n\
+        Input of ProbMetab or CAMERA.combinexcAnnos"
+
+    # TODO: sniff
 
 
-class RdataCameraNegative( Binary ):
+class RdataCameraNegative(RData):
     file_ext = "rdata.camera.negative"
-    blurb = "Output of CAMERA.annotate using negatif acquisition mode\nInput of ProbMetab or CAMERA.combinexcAnnos"
-    
-    #TODO: sniff
+    blurb = "Output of CAMERA.annotate using negatif acquisition mode\n\
+        Input of ProbMetab or CAMERA.combinexcAnnos"
+
+    # TODO: sniff
 
 
-class RdataCameraQuick( Binary ):
+class RdataCameraQuick(RData):
     file_ext = "rdata.camera.quick"
     blurb = "Output of CAMERA.annotate using the quick mode"
-    
-    #TODO: sniff
 
-
+    # TODO: sniff
