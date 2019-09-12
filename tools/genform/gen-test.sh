c619cc35ee18869a3f617d8c406b5151e45f2ddb
huberc <carolin-elisabeth.huber@ufz.de>
2019-09-12 13:23:06 +0200

    First commit genform

---
+++ tools/genform/gen-test.sh
@@ -0,0 +1,2 @@
+genform ms=test-data/CreatineMs.txt out=test-data/CreatineMs.out exist out=test-data/CreatineMs.out acc=20 rej=30 
+genform ms=test-data/CreatineMs.txt msms=test-data/CreatineMsMs.txt exist oms=test-data/CreatineMsMs-oms.out omsms=test-data/CreatineMsMs-omsms.out oclean=test-data/CreatineMsMs-oclean.out out=test-data/CreatineMsMs.out acc=20 rej=30 
