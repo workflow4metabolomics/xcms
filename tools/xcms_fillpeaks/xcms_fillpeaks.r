a2a79ab874880c4416bdb087ce0b82f00f6ee7da
lecorguille <gildas.le.corguille@gmail.com>
2021-03-08 22:29:52 +0100

    xcms_* - lintr - There should be a space between right parenthesis and an opening curly brace

--- tools/xcms_fillpeaks/xcms_fillpeaks.r
+++ tools/xcms_fillpeaks/xcms_fillpeaks.r
@@ -10,7 +10,9 @@ sink(log_file, type = "output")
 cat("\tSESSION INFO\n")
 
 #Import the different functions
-source_local <- function(fname){ argv <- commandArgs(trailingOnly = FALSE); base_dir <- dirname(substring(argv[grep("--file=", argv)], 8)); source(paste(base_dir, fname, sep = "/")) }
+source_local <- function(fname) {
+  argv <- commandArgs(trailingOnly = FALSE); base_dir <- dirname(substring(argv[grep("--file=", argv)], 8)); source(paste(base_dir, fname, sep = "/"))
+}
 source_local("lib.r")
 
 pkgs <- c("xcms", "batch")
