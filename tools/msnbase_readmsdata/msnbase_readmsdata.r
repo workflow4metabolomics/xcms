bfd0da28dd1e21f46fcd6c2b99695418a9ce9efd
lecorguille <gildas.le.corguille@gmail.com>
2021-04-08 17:46:47 +0200

    readmsdata - fix lintr

--- tools/msnbase_readmsdata/msnbase_readmsdata.r
+++ tools/msnbase_readmsdata/msnbase_readmsdata.r
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 
 # ----- LOG FILE -----
-log_file <- file("log.txt", open="wt")
+log_file <- file("log.txt", open = "wt")
 sink(log_file)
 sink(log_file, type = "output")
 
@@ -10,10 +10,12 @@ sink(log_file, type = "output")
 cat("\tSESSION INFO\n")
 
 #Import the different functions
-source_local <- function(fname){ argv <- commandArgs(trailingOnly=FALSE); base_dir <- dirname(substring(argv[grep("--file=", argv)], 8)); source(paste(base_dir, fname, sep="/")) }
+source_local <- function(fname) {
+  argv <- commandArgs(trailingOnly = FALSE); base_dir <- dirname(substring(argv[grep("--file=", argv)], 8)); source(paste(base_dir, fname, sep = "/"))
+}
 source_local("lib.r")
 
-pkgs <- c("MSnbase","batch")
+pkgs <- c("MSnbase", "batch")
 loadAndDisplayPackages(pkgs)
 cat("\n\n");
 
@@ -21,7 +23,7 @@ cat("\n\n");
 # ----- ARGUMENTS -----
 cat("\tARGUMENTS INFO\n")
 args <- parseCommandArgs(evaluate = FALSE) #interpretation of arguments given in command line as an R list of objects
-write.table(as.matrix(args), col.names=F, quote=F, sep='\t')
+write.table(as.matrix(args), col.names = F, quote = F, sep = "\t")
 
 cat("\n\n")
 
@@ -55,23 +57,19 @@ cat("\tMAIN PROCESSING INFO\n")
 cat("\t\tCOMPUTE\n")
 
 cat("\t\t\tCreate a phenodata data.frame\n")
-s_groups <- sapply(files, function(x) tail(unlist(strsplit(dirname(x),"/")), n=1))
+s_groups <- sapply(files, function(x) tail(unlist(strsplit(dirname(x), "/")), n = 1))
 s_name <- tools::file_path_sans_ext(basename(files))
-pd <- data.frame(sample_name=s_name, sample_group=s_groups, stringsAsFactors=FALSE)
+pd <- data.frame(sample_name = s_name, sample_group = s_groups, stringsAsFactors = FALSE)
 print(pd)
 
 cat("\t\t\tLoad Raw Data\n")
-raw_data <- readMSData(files=files, pdata = new("NAnnotatedDataFrame", pd), mode="onDisk")
+raw_data <- readMSData(files = files, pdata = new("NAnnotatedDataFrame", pd), mode = "onDisk")
 
 # Transform the files absolute pathways into relative pathways
-raw_data@processingData@files <- sub(paste(getwd(), "/", sep="") , "", raw_data@processingData@files)
+raw_data@processingData@files <- sub(paste(getwd(), "/", sep = ""), "", raw_data@processingData@files)
 
 # Create a sampleMetada file
-sampleNamesList <- getSampleMetadata(xdata=raw_data, sampleMetadataOutput="sampleMetadata.tsv")
-
-#cat("\t\t\tCompute and Store TIC and BPI\n")
-#chromTIC <- chromatogram(raw_data, aggregationFun = "sum")
-#chromBPI <- chromatogram(raw_data, aggregationFun = "max")
+sampleNamesList <- getSampleMetadata(xdata = raw_data, sampleMetadataOutput = "sampleMetadata.tsv")
 
 cat("\n\n")
 
@@ -85,7 +83,7 @@ cat("\n\n")
 
 #saving R data in .Rdata file to save the variables used in the present tool
 objects2save <- c("raw_data", "zipfile", "singlefile", "md5sumList", "sampleNamesList") #, "chromTIC", "chromBPI")
-save(list=objects2save[objects2save %in% ls()], file="readmsdata.RData")
+save(list = objects2save[objects2save %in% ls()], file = "readmsdata.RData")
 
 
 cat("\tDONE\n")
