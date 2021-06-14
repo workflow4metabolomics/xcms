191ae459bacf7a9f7b0c2aaeecf5e342fdf3e623
Art Eschenlauer <esch0041@umn.edu>
2021-06-14 18:16:32 +0000

    fixed failed planemo test

--- tools/xcms_refine/xcms_refine.r
+++ tools/xcms_refine/xcms_refine.r
@@ -12,7 +12,7 @@ cat("\tSESSION INFO\n")
 #Import the different functions
 source_local <- function(fname) {
   argv <- commandArgs(trailingOnly = FALSE)
-  base_dir <- dirname(substring(argv[grep("--file = ", argv)], 8))
+  base_dir <- dirname(substring(argv[grep("--file=", argv)], 8))
   source(paste(base_dir, fname, sep = "/"))
 }
 source_local("lib.r")
